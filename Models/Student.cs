b5158afe8f166b79412f55f0f5de837fbdd0a48a
Clóvis Daniel Costa <clovisdaniel@hotmail.com>
2024-02-06 19:46:03 -0300

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Poc.MongoDB.Api.Models;
+
+public class Student
+{
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public bool IsGraduated { get; set; }
+    public string[]? Courses { get; set; }
+    public string Gender { get; set; } = string.Empty;
+    public int Age { get; set; }
+}
