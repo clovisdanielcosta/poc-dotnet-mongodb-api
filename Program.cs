eae9ee9e5d9ab5dcd0be0ad7837cc9522244c496
Clóvis Daniel Costa <clovisdaniel@hotmail.com>
2024-02-06 22:34:52 -0300

    Configuring GetSection + Dependency Injection on Program

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.Extensions.Options;
 using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using Poc.MongoDB.Api.Interfaces;
 using Poc.MongoDB.Api.Models;
 using System;
 
@@ -6,12 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-var app = builder.Build();
+builder.Services.Configure<DbSchoolDatabaseSettings>(builder.Configuration.GetSection(nameof(DbSchoolDatabaseSettings)));
+builder.Services.AddSingleton<IDbSchoolDatabaseSettings>(sp => sp.GetRequiredService<IOptions<DbSchoolDatabaseSettings>>().Value);
+builder.Services.AddSingleton<IMongoClient>(s => new MongoClient(builder.Configuration.GetValue<string>("DbSchoolDatabaseSettings:ConnectionString")));
 
+builder.Services.AddScoped<IStudentService, IStudentService>();
 
 BsonClassMap.RegisterClassMap<Student>(classMap =>
 {
@@ -23,8 +25,13 @@ BsonClassMap.RegisterClassMap<Student>(classMap =>
     classMap.MapMember(p => p.Age);
 });
 
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
