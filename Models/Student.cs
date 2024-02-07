b5a1e5e3b326967a5850525c0106a8aa62a5808b
Clóvis Daniel Costa <clovisdaniel@hotmail.com>
2024-02-06 22:03:37 -0300

    Added Map for Mongo on Program + appsettings db configuration

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,11 +1,14 @@
-﻿namespace Poc.MongoDB.Api.Models;
+﻿using MongoDB.Bson.Serialization.Attributes;
 
+namespace Poc.MongoDB.Api.Models;
+
+[BsonIgnoreExtraElements]
 public class Student
 {
     public string Id { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public bool IsGraduated { get; set; }
-    public string[]? Courses { get; set; }
+    public List<string>? Courses { get; set; }
     public string Gender { get; set; } = string.Empty;
     public int Age { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Bson.Serialization;
+using Poc.MongoDB.Api.Models;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +10,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
+
+
+BsonClassMap.RegisterClassMap<Student>(classMap =>
+{
+    classMap.MapMember(p => p.Id);
+    classMap.MapMember(p => p.Name);
+    classMap.MapMember(p => p.IsGraduated);
+    classMap.MapMember(p => p.Courses);
+    classMap.MapMember(p => p.Gender);
+    classMap.MapMember(p => p.Age);
+});
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
